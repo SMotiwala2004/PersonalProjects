14e6aa3c25bc17147df3025ec70b404cd28d9a40
Safeer Motiwala <54222043+SMotiwala2004@users.noreply.github.com>
2024-07-14 21:10:09 -0500

    Add files via upload

---
+++ AlarmClock/index.js
@@ -0,0 +1,5 @@
+let hours = document.getElementById("Hours");
+let minutes = document.getElementById("Minutes");
+let seconds = document.getElementById("Seconds");
+let start = document.getElementById("Start");
+let stop = document.getElementById("Stop");
