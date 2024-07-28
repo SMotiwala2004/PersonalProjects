deb5c67bd714f31373c48da5a1a66fc4a2642698
Safeer Motiwala <54222043+SMotiwala2004@users.noreply.github.com>
2024-07-27 23:40:30 -0500

    Add files via upload

--- TDEE_Calculator/script.js
+++ TDEE_Calculator/script.js
@@ -3,19 +3,39 @@ let weight = document.getElementById("weight");
 let height = document.getElementById("height");
 let age = document.getElementById("age");
 let generate = document.getElementById("calculate");
+let multiply = document.getElementById("multiplier")
+let kcal = document.getElementById("kcal-burned")
+
 let burned = 0;
+let multiplier;
 
 
 generate.onclick = function() {
+    if(multiply.value === "Sedentary") {
+        multiplier = 1.2;
+    }
+    if(multiply.value === "Lightly-Active") {
+        multiplier = 1.375;
+    }
+    if(multiply.value === "Moderately-Active"){
+        multiplier = 1.55;
+    }
+    if(multiply.value === "Active"){
+        multiplier = 1.725;
+    }
+    if(multiply.value === "Very-Active"){
+        multiplier = 1.9;
+    }
+
     if(age.value <= 0 || weight.value <= 0 || height.value <= 0) {
         window.alert("Cannot enter a negative value");
         event.preventDefault();
     } 
     if(sex.value === "Male") {
-            burned = Math.ceil((10*weight.value) + (6.25*height.value) - (5*age.value) + 5);
-            console.log(burned);
+            burned = Math.floor(multiplier*((10*weight.value) + (6.25*height.value) - (5*age.value) + 5));
+            kcal.textContent = burned;
     } else if(sex.value === "Female") {
-        burned = Math.ceil(10*weight.value) + (6.25*height.value) - (5*age.value) - 161;
-        console.log(burned);
+        burned = Math.floor(multiplier*((10*weight.value) + (6.25*height.value) - (5*age.value) - 161));
+        kcal.textContent = burned;
     }
 }
