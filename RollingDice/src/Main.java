8c1ae7b35f2ed251211cc1a29051e5c320a90d56
Safeer Motiwala <54222043+SMotiwala2004@users.noreply.github.com>
2024-08-15 17:18:59 -0500

    Add files via upload

--- RollingDice/src/Main.java
+++ RollingDice/src/Main.java
@@ -14,12 +14,14 @@ public class Main {
         String userInput = sc.nextLine();
 
         if(userInput.equalsIgnoreCase("Play")) {
+            //Call Rolling Dice Method
             RollingDice();
             System.out.println("Enter your guess from 1-6");
             guess = sc.nextInt();
             sc.nextLine();
 
             if(guess == diceNum) {
+                //Check if the guess is equal to the number rolled
                 points += 10;
                 System.out.println("You have " + points +" points");
             }
@@ -30,16 +32,19 @@ public class Main {
             MainMenu();
 
         } else if(userInput.equalsIgnoreCase("Exit")) {
+            //Program Exit
             System.out.println("You finished with a score of " + points);
             System.out.println("Exiting Game");
             System.exit(0);
         } else {
+            //Recieve invalid input
             System.out.println("Invalid Input");
             MainMenu();
         }
     }
 
     private static void RollingDice() {
+        //Roll dice from 1-6
         diceNum = (int)(Math.random()*6) +1;
         System.out.println("The dice has been rolled");
     }
